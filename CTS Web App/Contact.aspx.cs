88c15ff6ad49a980d8e90a6dbc573064afff606e
Barry Crossonuser.name <barry.crosson@computershare.ie>
2018-09-19 17:33:57 +0100

    Test CI + DC

--- CTS Web App/Contact.aspx.cs
+++ CTS Web App/Contact.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace CTS_Web_App
-{
+{ 
     public partial class Contact : Page
     {
         protected void Page_Load(object sender, EventArgs e)
